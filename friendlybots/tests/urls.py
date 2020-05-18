25ad1f3db56d257bc8336fd49e38bcaa5dcfcd98
abstract-thoery <none>
2020-05-18 15:58:24 -0700

    Update tests to reflect changes in friendlybots/views.py.

--- friendlybots/tests/urls.py
+++ friendlybots/tests/urls.py
@@ -4,11 +4,10 @@ if django.VERSION[0] >= 2:
 else:
     from django.conf.urls import url as version_url_path
 
-
-from ..views import FriendlyBotsView
+from ..views import FriendlyBots_as_view
 from .views import view_search_bots_only
 
 urlpatterns = [
-    version_url_path(r'^test_as_view/$', FriendlyBotsView.as_view(template_name='hello-friendly-bots.html')),
+    version_url_path(r'^test_as_view/$', FriendlyBots_as_view(template_name='hello-friendly-bots.html')),
     version_url_path(r'^test_view_decorator/$', view_search_bots_only),
 ]
